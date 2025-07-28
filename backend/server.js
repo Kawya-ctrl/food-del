103705405b87167d5fd0f42d5f24048a4efe125a
Kawya8688 <153253027+Kawya-ctrl@users.noreply.github.com>
2025-07-28 14:32:32 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = "https://food-del-backend-m03o.onrender.com";
 
 // Middleware FIRST
 app.use(cors());
